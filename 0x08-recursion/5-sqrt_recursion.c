e1ea95ee8868aa3e206692aaa7d15f0fc0042065
odalson <melvin4gups@gmail.com>
2023-07-05 05:14:36 -0700

    6-is_prime_number.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-int _sqrt_recursion(int n, int i);
+int actual _sqrt_recursion(int n, int i);
 
 /**
  * _sqrt_recursion - Returns the natural square root of a number
@@ -11,7 +11,9 @@ int _sqrt_recursion(int n, int i);
  */
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n, 1));
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
 }
 
 /**
@@ -21,15 +23,11 @@ int _sqrt_recursion(int n)
  *
  * Return: the natural root
  */
-int _sqrt(int n, int i)
+int actual_sqrt_recursion(int n, int i)
 {
-	int sqrt = i * i;
-
-	if (sqrt > n)
+	if (i * i > n)
 		return (-1);
-
-	if (sqrt == n)
+	if (i * i == n)
 		return (i);
-
-	return (_sqrt(n, i + 1));
+	return (actual_sqrt_recursion(n, i + 1));
 }
