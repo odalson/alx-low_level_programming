76383edbab1e1dce8a67e9f12ccf5ccb11354d3a
odalson <melvin4gups@gmail.com>
2023-09-04 10:11:27 -0700

    03

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -22,7 +22,7 @@ int _atoi(char *s)
 
 	while (i < len && f == 0)
 	{
-		if (s[i] == ' ')
+		if (s[i] == '-')
 			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
@@ -32,8 +32,7 @@ int _atoi(char *s)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9'
-
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
@@ -52,7 +51,7 @@ int _atoi(char *s)
  *
  * Return: 0 (Success), 1 (Error)
  */
-int main(int argc, char 8argv[])
+int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
