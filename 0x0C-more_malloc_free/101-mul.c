f5e178bc9badd37a350ab6c98d6f793d3b29dfa3
odalson <melvin4gups@gmail.com>
2023-09-07 05:20:20 -0700

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,8 @@
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+
+#define ERR_MSG "Error"
 
 /**
  * is_digit - checks if a string contains a non-digit char
@@ -48,7 +50,7 @@ void errors(void)
 }
 
 /**
- * main - multiplies two positive numbmers
+ * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
  *
@@ -57,39 +59,39 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	int length1, length2, length, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
+	length1 = _strlen(s1);
+	length2 = _strlen(s2);
+	length = length1 + length2 + 1;
+	result = malloc(sizeof(int) * length);
 	if (!result)
 		return (1);
-	for (i = 0; i <= len1 + len2; i++)
+	for (i = 0; i <= length1 + length2; i++)
 		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+	for (length1 = length1 - 1; length1 >= 0; length1--)
 	{
-		digit1 = s1[len1] - '0';
+		digit1 = s1[length1] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		for (length2 = _strlen(s2) - 1; length2 >= 0; length2--)
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
+			digit2 = s2[length2] - '0';
+			carry += result[length1 + length2 + 1] + (digit1 * digit2);
+			result[length1 + length2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+			result[length1 + length2 + 1] += carry;
 	}
-	for (i = 0; i < len - 1; i++)
+	for (i = 0; i < length - 1; i++)
 	{
 		if (result[i])
 			a = 1;
 		if (a)
-			_putchar(result[i]  + '0');
+			_putchar(result[i] + '0');
 	}
 	if (!a)
 		_putchar('0');
