b7ae42944478d8472b69ef8561ed859266103a06
odalson <melvin4gups@gmail.com>
2024-01-11 17:17:51 -0800

    updated

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,7 +8,7 @@
  * @n: value of the element
  * Return: the address of the new element
  */
-dlistint_t *add_dnodeint_end(dlist_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *h;
 	dlistint_t *new;
